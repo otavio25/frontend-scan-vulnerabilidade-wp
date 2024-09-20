7cec82299f4fcc19722c4083226892f2943e6468
otavio25 <souzaotavio721@gmail.com>
2024-09-20 12:36:17 -0300

    refactor: Adiciona responsividade no navbar, footer e imagem da pag home

--- frontend/src/app/_components/footer/footer.component.ts
+++ frontend/src/app/_components/footer/footer.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  showFooterA: boolean = false;
+  
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        // Verifica o path atual
+        this.showFooterA = event.url === '/';
+      });
+  }
 }
